103033d4d4df7ca69e29a71b80cb26393a326101
RDisquete <rafael.doradozamoro@gmail.com>
2026-06-26 18:53:45 +0200

    Update PassCard.tsx

--- src/pages/Access/components/PassCard.tsx
+++ src/pages/Access/components/PassCard.tsx
@@ -36,13 +36,13 @@ export default function PassCard({ passPhoto, passNumber, passName, loading }: P
         <div className="absolute w-14 h-14 rounded-full border-[3px] left-[55%] bottom-[38%] z-30" style={{ borderColor: '#D4A373' }} />
         <SolidLine className="w-24 h-0.75 left-[42%] top-[14%] z-30 -rotate-6" style={{ backgroundColor: '#e85d6f' }} />
         <SolidLine className="w-16 h-0.5 left-[8%] top-[22%] z-30 rotate-12" style={{ backgroundColor: '#D4A373' }} />
-        <SolidLine className="w-20 h-0.5 right-[4%] top-[52%] z-30 -rotate-[20deg]" style={{ backgroundColor: 'rgba(255,255,255,0.15)' }} />
+        <SolidLine className="w-20 h-0.5 right-[4%] top-[52%] z-30 rotate-[-20deg]" style={{ backgroundColor: 'rgba(255,255,255,0.15)' }} />
         <SolidTri className="w-10 h-10 left-[10%] bottom-[30%] z-30 rotate-30" style={{ backgroundColor: '#e85d6f' }} />
-        <SolidTri className="w-6 h-6 left-[5%] top-[6%] z-30 rotate-[60deg]" style={{ backgroundColor: 'rgba(212,163,115,0.4)' }} />
+        <SolidTri className="w-6 h-6 left-[5%] top-[6%] z-30 rotate-60" style={{ backgroundColor: 'rgba(212,163,115,0.4)' }} />
         <SolidDot className="w-5 h-5 left-[38%] bottom-[12%] z-30" style={{ backgroundColor: '#D4A373' }} />
         <SolidDot className="w-3 h-3 left-[15%] top-[45%] z-30" style={{ backgroundColor: 'rgba(232,93,111,0.3)' }} />
         <SolidBox className="w-7 h-7 right-[6%] bottom-[18%] z-30 rotate-[-15deg]" style={{ backgroundColor: '#e85d6f' }} />
-        <SolidBox className="w-4 h-4 left-[28%] top-[30%] z-30 rotate-[40deg]" style={{ backgroundColor: 'rgba(255,255,255,0.1)' }} />
+        <SolidBox className="w-4 h-4 left-[28%] top-[30%] z-30 rotate-40" style={{ backgroundColor: 'rgba(255,255,255,0.1)' }} />
         <SolidLine className="w-32 h-0.75 left-[20%] top-[68%] z-30 rotate-2" style={{ backgroundColor: '#3a1a4a' }} />
         <div className="absolute left-[50%] top-[22%] z-30 w-1 h-1 rounded-full" style={{ backgroundColor: 'rgba(255,255,255,0.2)' }} />
         <div className="absolute left-[52%] top-[24%] z-30 w-0.5 h-0.5 rounded-full" style={{ backgroundColor: 'rgba(255,255,255,0.15)' }} />
@@ -56,7 +56,7 @@ export default function PassCard({ passPhoto, passNumber, passName, loading }: P
             </span>
             <span className="font-heading text-[clamp(5rem,18vw,12rem)] font-black tracking-[-0.15em] block -mt-1 text-white/70"
                   style={{ textShadow: '5px 5px 0 #e85d6f, 8px 8px 0 rgba(0,0,0,0.25)' }}>
-              on<span className="text-[#e85d6f]">i</span>a
+              on<span className="text-coral">i</span>a
             </span>
           </div>
         </div>
